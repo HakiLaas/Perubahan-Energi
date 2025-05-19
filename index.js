f2fc7e6cdce202fdfec1b46679b4817bdfd7533d
HakiLaas <gtcammie01@gmail.com>
2025-05-19 18:35:55 +0800

    Something Changes

---
+++ index.js
@@ -0,0 +1,54 @@
+    // Hamburger Menu
+const toggleBtn = document.querySelector('.togglebtn');
+const navLinks = document.querySelector('.nav-link');
+
+toggleBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+});
+
+    // Button Next dan Home
+document.addEventListener("DOMContentLoaded", () => {
+    const nextSection = document.querySelector(".next-section a");
+    const aboutSection = document.getElementById("about");
+    const arrowIcon = nextSection.querySelector("i");
+    
+    window.addEventListener("scroll", () => {
+        const aboutTop = aboutSection.getBoundingClientRect().top;
+    
+        if (aboutTop <= 100) {
+            arrowIcon.classList.remove("bx-chevron-down");
+            arrowIcon.classList.add("bx-chevron-up");
+            nextSection.href = "#"; 
+        } else {
+            arrowIcon.classList.remove("bx-chevron-up");
+            arrowIcon.classList.add("bx-chevron-down");
+            nextSection.href = "#about"; 
+        }
+    });
+});
+
+// Dark Mode Light Mode
+document.addEventListener("DOMContentLoaded", () => {
+    const themeToggle = document.querySelectorAll("#theme, #sun");
+    const body = document.body;
+
+    const Icon = () => {
+        themeToggle.forEach(btn => {
+            if (body.classList.contains("dark")) {
+                btn.classList.remove("bx-sun");
+                btn.classList.add("bx-moon");
+            } else {
+                btn.classList.remove("bx-moon");
+                btn.classList.add("bx-sun");
+            }
+        });
+    };
+
+    themeToggle.forEach(toggle => {
+        toggle.addEventListener("click", () => {
+            body.classList.toggle("dark"); 
+            Icon();
+        });
+    });
+    Icon();
+});
